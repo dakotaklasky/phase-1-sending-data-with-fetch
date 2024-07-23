8d6055d0c7826498b921c981e5abe92a8e13f515
Dakota <dakota.klasky@gmail.com>
2024-07-23 17:36:23 -0400

    create function

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+
+    const formData ={
+        name: userName,
+        email: userEmail
+    }
+
+    const configurationObject = {
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+        }
+
+    
+    const response = fetch("http://localhost:3000/users",configurationObject)
+    
+    const divElement = document.createElement('div')
+    document.body.appendChild(divElement)
+
+    response.then((r) => r.json())
+    .then((j) => divElement.append(j.id))
+    .catch((error) => divElement.append(error.message))
+
+    return response
+}
+
+
